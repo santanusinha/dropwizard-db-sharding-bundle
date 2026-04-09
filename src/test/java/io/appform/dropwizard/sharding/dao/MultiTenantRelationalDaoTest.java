a67216f71b9cfd2b5b5f2adade4d8a028a08d2ab
Debashis Karmakar <debashis.karmakar@phonepe.com>
2026-04-09 18:46:51 +0530

    fix: remove public modifier from JUnit 5 test methods (SonarCloud code smells)

--- src/test/java/io/appform/dropwizard/sharding/dao/MultiTenantRelationalDaoTest.java
+++ src/test/java/io/appform/dropwizard/sharding/dao/MultiTenantRelationalDaoTest.java
@@ -327,7 +327,7 @@ public class MultiTenantRelationalDaoTest {
   }
 
   @Test
-  public void testScrollingWithQuerySpec() {
+  void testScrollingWithQuerySpec() {
     val ids = new HashSet<String>();
     IntStream.range(1, 1_000)
         .forEach(i -> {
